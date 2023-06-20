b3b843b7001d12d6a6a1b2d7d9d3e0c3686ec128
muflikhandimasd <muflikhan.dimas@gmail.com>
2023-06-21 00:58:38 +0300

    first commit

---
+++ initializers/db.go
@@ -0,0 +1,33 @@
+package initializers
+
+import (
+	"basic-golang-fiber/models"
+	"fmt"
+	"log"
+	"os"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var DB *gorm.DB
+
+func ConnectDB(config *Config) {
+	var err error
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", config.DBUserName, config.DBUserPassword, config.DBHost, config.DBPort, config.DBName)
+
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to the Database! \n", err.Error())
+		os.Exit(1)
+	}
+
+	DB.Exec("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\"")
+	DB.Logger = logger.Default.LogMode(logger.Info)
+
+	log.Println("Running Migrations")
+	DB.AutoMigrate(&models.Note{})
+
+	log.Println("🚀 Connected Successfully to the Database")
+}
